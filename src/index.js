bd89782fa5e56b881a756fad857dabda8276c8f0
Ivan <kan.ivann@gmail.com>
2020-03-17 11:00:57 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let y = Math.abs(n)
+    let a = String(y)
+    var b = a.split("");
+    var arr = b.reverse();
+    var aar = arr.join("");
+    var res = Number(aar);
+    return res;
 }
